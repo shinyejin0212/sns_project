57787e3b60baffacfd3a21dc0324414026fe2a2f
shinyejin0212 <shinyejin0212@gmail.com>
2021-07-13 22:05:01 +0900

    07_13_회원가입수정

--- firstproject/firstproject/settings.py
+++ firstproject/firstproject/settings.py
@@ -148,3 +148,8 @@ AUTHENTICATION_BACKENDS = [
 
 SITE_ID = 1
 LOGIN_REDIRECT_URL = '/'
+
+DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+ACCOUNT_EMAIL_VERIFICATION = "none"
